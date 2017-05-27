2ba73542d8485a35cab5970c29e7c89f4a153759
Markus Mayer <widemeadows@gmail.com>
2017-05-27 18:04:37 +0200

    Update comments and functions names

--- main.cpp
+++ main.cpp
@@ -72,16 +72,14 @@ static inline constexpr size_t megabyte(const size_t n) {
     return kilobyte(n * 1024UL);
 }
 
-float dot_product_avx(const float* const a_row, const float* const b_row, const size_t N) {
+float dot_product_avx256(const float *const a_row, const float *const b_row, const size_t N) {
     auto total = _mm256_set1_ps(0.0f);
     for (size_t i = 0; i < N; i += 32) {
         // Prefetch the next batch into L2 - saves around 40ms on 2 million 2048-float rows.
         _mm_prefetch(&a_row[i + 32 * 8], _MM_HINT_T1);
         _mm_prefetch(&b_row[i + 32 * 8], _MM_HINT_T1);
 
-        // load 32 floats per vector
-        // For some reason, _mm256_loadu_ps is faster than _mm256_load_ps on both AVX and AVX2 ...
-        // In this example, times drop from 160ms to 120ms ... ?
+        // Load 32 floats per vector.
         const auto a0 = _mm256_load_ps(&a_row[i]);
         const auto b0 = _mm256_load_ps(&b_row[i]);
         const auto a1 = _mm256_load_ps(&a_row[i + 8]);
@@ -91,17 +89,17 @@ float dot_product_avx(const float* const a_row, const float* const b_row, const 
         const auto a3 = _mm256_load_ps(&a_row[i + 24]);
         const auto b3 = _mm256_load_ps(&b_row[i + 24]);
 
-        // do separate dot products
+        // Do separate dot products.
         const auto c0 = _mm256_dp_ps(a0, b0, 0xff);
         const auto c1 = _mm256_dp_ps(a1, b1, 0xff);
         const auto c2 = _mm256_dp_ps(a2, b2, 0xff);
         const auto c3 = _mm256_dp_ps(a3, b3, 0xff);
 
-        // do separate partial sums
+        // Do separate partial sums.
         const auto p0 = _mm256_add_ps(c0, c1);
         const auto p1 = _mm256_add_ps(c2, c3);
 
-        // aggregate the partial sums and allocate to running total
+        // Aggregate the partial sums and allocate to running total.
         const auto s = _mm256_add_ps(p0, p1);
         total = _mm256_add_ps(total, s);
     }
@@ -239,7 +237,7 @@ int what() {
 
             // Calculate the dot product of the 2048-element vector
             static_assert((N & 31) == 0, "Vector length must be a multiple of 32 elements.");
-            const auto dot_product = dot_product_avx(a_row, b_row, N);
+            const auto dot_product = dot_product_avx256(a_row, b_row, N);
             // const auto dot_product = dot_product_naive(a_row, b_row, N);
             // const auto dot_product = dot_product_unrolled_8(a_row, b_row, N);
 
