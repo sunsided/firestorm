494d0a1681c9dd20ff81774a07c329203e3932b6
Markus Mayer <widemeadows@gmail.com>
2017-05-27 17:18:22 +0200

    Fix implementation of block manager backed dot

--- main.cpp
+++ main.cpp
@@ -79,10 +79,10 @@ int what() {
     auto random = std::bind(distribution, generator);
 
     const size_t N = 2048;
-    const size_t M = 10000;
+    const size_t M = 100000;
 
     auto expected = new float[M];
-    auto sum = new float[M];
+    auto result = new float[M];
 
     std::cout << "Initializing vectors ..." << std::endl;
     BlockManager blockManager_a;
@@ -97,6 +97,9 @@ int what() {
     mem_block_t* block_b = nullptr;
     size_t remaining_block_size = 0;    // number of remaining bytes in the current block
     size_t float_offset = 0;            // index into the current buffer, counts floats
+
+    // Keep track of the total sum for validation.
+    auto expected_total_sum = 0.0f;
 
     for (size_t j = 0; j < M; ++j) {
 
@@ -130,83 +133,101 @@ int what() {
             a[i] = random();
             b[i] = random();
             expected[j] += a[i] * b[i];
+            expected_total_sum += a[i] * b[i];
         }
     }
     std::cout << "- " << M << "/" << M << std::endl
               << "Vectors initialized." << std::endl;
 
-    std::cout << "Running tests ..." << std::endl;
-    for (size_t repetition = 0; repetition < 20; ++repetition)
+    const size_t repetitions = 20;
+    for (size_t repetition = 0; repetition < repetitions; ++repetition)
     {
+        std::cout << "Running test round " << (repetition+1) << " of " << repetitions << " ..." << std::endl;
+        size_t current_block = 0;
+        block_a = blockManager_a.get(current_block);
+        block_b = blockManager_b.get(current_block);
+        float_offset = 0;
+
+        const auto vectors_per_block = block_size / (N * sizeof(float));
+        auto remaining_vectors_per_block = vectors_per_block;
+
+        // Keep track of the total sum for validation.
+        auto total_sum = 0.0f;
+
         auto start_time = std::chrono::high_resolution_clock::now();
-        const auto blocks = blockManager_a.size();
 
         // Run for a couple of test iterations ...
-        for (size_t test_iteration = 0; test_iteration < M; ++test_iteration) {
-            // Go through each data block / chunk ...
-            for (size_t block = 0; block < blocks; ++block) {
-                block_a = blockManager_a.allocate(block);
-                block_b = blockManager_b.allocate(block);
+        for (size_t vector_idx = 0; vector_idx < M; ++vector_idx) {
 
-                for (remaining_block_size = block_size, float_offset = 0;
-                     remaining_block_size >= N;
-                     remaining_block_size -= N, float_offset += N) {
+            if (remaining_vectors_per_block == 0) {
+                current_block += 1;
+                block_a = blockManager_a.get(current_block);
+                block_b = blockManager_b.get(current_block);
 
-                    auto a_row = &block_a->data[float_offset];
-                    auto b_row = &block_a->data[float_offset];
-                    sum[test_iteration] = 0.0f;
-
-                    auto total = _mm256_set1_ps(0.0f);
-
-                    static_assert((N & 31) == 0, "Vector length must be a multiple of 32 elements.");
-                    for (size_t i = 0; i < N; i += 32) {
-                        // prefetch the next batch into L2 - saves around 40ms on 2 million 2048-float rows
-                        _mm_prefetch(&a_row[i + 32 * 8], _MM_HINT_T1);
-                        _mm_prefetch(&b_row[i + 32 * 8], _MM_HINT_T1);
-
-                        // load 32 floats per vector
-                        // For some reason, _mm256_loadu_ps is faster than _mm256_load_ps on both AVX and AVX2 ...
-                        // In this example, times drop from 160ms to 120ms ... ?
-                        const auto a0 = _mm256_load_ps(&a_row[i]);
-                        const auto b0 = _mm256_load_ps(&b_row[i]);
-                        const auto a1 = _mm256_load_ps(&a_row[i + 8]);
-                        const auto b1 = _mm256_load_ps(&b_row[i + 8]);
-                        const auto a2 = _mm256_load_ps(&a_row[i + 16]);
-                        const auto b2 = _mm256_load_ps(&b_row[i + 16]);
-                        const auto a3 = _mm256_load_ps(&a_row[i + 24]);
-                        const auto b3 = _mm256_load_ps(&b_row[i + 24]);
-
-                        // do separate dot products
-                        const auto c0 = _mm256_dp_ps(a0, b0, 0xff);
-                        const auto c1 = _mm256_dp_ps(a1, b1, 0xff);
-                        const auto c2 = _mm256_dp_ps(a2, b2, 0xff);
-                        const auto c3 = _mm256_dp_ps(a3, b3, 0xff);
-
-                        // do separate partial sums
-                        const auto p0 = _mm256_add_ps(c0, c1);
-                        const auto p1 = _mm256_add_ps(c2, c3);
-
-                        // aggregate the partial sums and allocate to running total
-                        const auto s = _mm256_add_ps(p0, p1);
-                        total = _mm256_add_ps(total, s);
-                    }
-
-                    alignas(32) float ptr[8];
-                    _mm256_store_ps(ptr, total);
-                    sum[test_iteration] = ptr[0] + ptr[5];
-                }
+                float_offset = 0;
+                remaining_vectors_per_block = vectors_per_block;
             }
+
+            auto a_row = &block_a->data[float_offset];
+            auto b_row = &block_b->data[float_offset];
+
+            --remaining_vectors_per_block;
+            float_offset += N;
+
+            result[vector_idx] = 0.0f;
+
+            // Calculate the dot product of the 2048-element vector
+            auto total = _mm256_set1_ps(0.0f);
+            static_assert((N & 31) == 0, "Vector length must be a multiple of 32 elements.");
+            for (size_t i = 0; i < N; i += 32) {
+                // Prefetch the next batch into L2 - saves around 40ms on 2 million 2048-float rows.
+                _mm_prefetch(&a_row[i + 32 * 8], _MM_HINT_T1);
+                _mm_prefetch(&b_row[i + 32 * 8], _MM_HINT_T1);
+
+                // load 32 floats per vector
+                // For some reason, _mm256_loadu_ps is faster than _mm256_load_ps on both AVX and AVX2 ...
+                // In this example, times drop from 160ms to 120ms ... ?
+                const auto a0 = _mm256_load_ps(&a_row[i]);
+                const auto b0 = _mm256_load_ps(&b_row[i]);
+                const auto a1 = _mm256_load_ps(&a_row[i + 8]);
+                const auto b1 = _mm256_load_ps(&b_row[i + 8]);
+                const auto a2 = _mm256_load_ps(&a_row[i + 16]);
+                const auto b2 = _mm256_load_ps(&b_row[i + 16]);
+                const auto a3 = _mm256_load_ps(&a_row[i + 24]);
+                const auto b3 = _mm256_load_ps(&b_row[i + 24]);
+
+                // do separate dot products
+                const auto c0 = _mm256_dp_ps(a0, b0, 0xff);
+                const auto c1 = _mm256_dp_ps(a1, b1, 0xff);
+                const auto c2 = _mm256_dp_ps(a2, b2, 0xff);
+                const auto c3 = _mm256_dp_ps(a3, b3, 0xff);
+
+                // do separate partial sums
+                const auto p0 = _mm256_add_ps(c0, c1);
+                const auto p1 = _mm256_add_ps(c2, c3);
+
+                // aggregate the partial sums and allocate to running total
+                const auto s = _mm256_add_ps(p0, p1);
+                total = _mm256_add_ps(total, s);
+            }
+
+            alignas(32) float ptr[8];
+            _mm256_store_ps(ptr, total);
+            result[vector_idx] = ptr[0] + ptr[5];
+            total_sum += ptr[0] + ptr[5];
         }
 
         auto end_time = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-        std::cout << "Sum: " << sum[0] << " (expected: " << expected[0] << ")" << " - duration: " << duration << "ms"
+        auto vectors_per_second = static_cast<float>(M * 1000) / static_cast<float>(duration);
+        std::cout << "Sum: " << total_sum << " (expected: " << expected_total_sum << ")" << " - duration: " << duration << "ms"
+                  << " (" << vectors_per_second << " vector/s)"
                   << std::endl;
     }
 
     std::cout << "Cleaning up ..." << std::endl;
     delete expected;
-    delete sum;
+    delete result;
 
     std::cout << "Done." << std::endl;
 }
