9b6b0262b9d453efdca8278a4245d8d7709fac0d
Markus Mayer <widemeadows@gmail.com>
2018-02-24 18:39:33 +0100

    Refactor main application

--- src/firestorm/src/main.cpp
+++ src/firestorm/src/main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <random>
-#include <chrono>
-#include <functional>
 #include <memory>
 
 #ifdef USE_PROFILER
 #include <gperftools/profiler.h>
 #endif
 
-#include <spdlog/spdlog.h>
 #include <logging/LoggerFactory.h>
-#include "firestorm/Simd.h"
-#include "firestorm/ChunkManager.h"
-#include "firestorm/Worker.h"
-#include "firestorm/DotProductVisitor.h"
-#include "firestorm/dot_product_naive.h"
+#include <firestorm/Simd.h>
+#include <firestorm/ChunkManager.h>
+#include <firestorm/Worker.h>
+#include <firestorm/DotProductVisitor.h>
+#include <firestorm/dot_product_naive.h>
 #if USE_AVX
-#include "firestorm/dot_product_avx256.h"
+#include <firestorm/dot_product_avx256.h>
 #endif
-#include "firestorm/dot_product_openmp.h"
-#include "firestorm/dot_product_sse42.h"
+#include <firestorm/dot_product_openmp.h>
+#include <firestorm/dot_product_sse42.h>
+
+#include "benchmark.h"
 
 using namespace std;
 namespace spd = spdlog;
@@ -28,15 +27,6 @@ namespace spd = spdlog;
 // TODO: Boost.SIMD
 // TODO: OpenMP backed loops
 // TODO: determine __restrict__ keyword support from https://github.com/elemental/Elemental/blob/master/cmake/detect/CXX.cmake
-
-const auto MS_TO_S = 1000.0F;
-
-const size_t NUM_DIMENSIONS = 2048;
-#if USE_AVX
-const size_t NUM_VECTORS = 100000;
-#else
-const size_t NUM_VECTORS = 5000;
-#endif
 
 vector_t create_query_vector(const shared_ptr<spdlog::logger> &log) {
     const auto seed = 0L;
@@ -60,122 +50,6 @@ vector_t create_query_vector(const shared_ptr<spdlog::logger> &log) {
 
     log->debug("Test vector norm before normalizing is {} ({} after that).", norm, norm2);
     return query;
-}
-
-template <typename T>
-void run_test_round(const shared_ptr<spdlog::logger> &log, const size_t repetitions, float *const result, const ChunkManager &chunkManager,
-                    const vector_t& query, const bytes_t chunk_size, const size_t expected_best_idx, float expected_best_score) {
-
-    static_assert(std::is_convertible<T*, dot_product_t*>::value, "Derived type must inherit dot_product_t as public");
-    const T calculate {};
-
-    const auto vectors_per_chunk = chunk_size / (NUM_DIMENSIONS * sizeof(float));
-    auto duration_ms = static_cast<size_t>(0);
-
-    for (size_t repetition = 0; repetition < repetitions; ++repetition) {
-        auto start_time = std::chrono::_V2::system_clock::now();
-
-        // Keep track of the total sum for validation.
-        auto best_match = 0.0f;
-        auto best_match_idx = static_cast<size_t>(0);
-
-        chunk_idx_t current_chunk = 0;
-        auto chunk = chunkManager.get_ro(current_chunk);
-        auto query_vector = query.data;
-
-        auto float_offset = 0;
-        auto remaining_vectors_per_chunk = vectors_per_chunk;
-
-        // Run over all vectors ...
-        for (size_t vector_idx = 0; vector_idx < NUM_VECTORS; ++vector_idx) {
-
-            if (remaining_vectors_per_chunk == 0) {
-                current_chunk += 1;
-                chunk = chunkManager.get_ro(current_chunk);
-
-                float_offset = 0;
-                remaining_vectors_per_chunk = vectors_per_chunk;
-            }
-
-            auto ref_vector = &chunk->data[float_offset];
-
-            --remaining_vectors_per_chunk;
-            float_offset += NUM_DIMENSIONS;
-
-            // Calculate the dot product of the 2048-element vector
-            static_assert((NUM_DIMENSIONS & 31) == 0, "Vector length must be a multiple of 32 elements.");
-            const auto dot_product = calculate(ref_vector, query_vector, NUM_DIMENSIONS);
-
-            result[vector_idx] = dot_product;
-
-            if (dot_product > best_match) {
-                best_match = dot_product;
-                best_match_idx = vector_idx;
-            }
-        }
-
-        auto end_time = std::chrono::_V2::system_clock::now();
-        auto local_duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-        duration_ms += local_duration_ms;
-
-        auto local_vectors_per_second = static_cast<float>(NUM_VECTORS) * MS_TO_S / static_cast<float>(local_duration_ms);
-        log->debug("- Round {}/{} matched {} at {} (expected {} at {}); took {} ms ({} vectors/s)",
-                   repetition + 1, repetitions,
-                   best_match, best_match_idx,
-                   expected_best_score, expected_best_idx,
-                   local_duration_ms, local_vectors_per_second);
-    }
-
-    const size_t num_vectors = repetitions * NUM_VECTORS;
-    auto vectors_per_second = static_cast<float>(num_vectors) * MS_TO_S / static_cast<float>(duration_ms);
-    log->info("- Processed {} vectors in {} ms ({} vectors/s)",
-              num_vectors, duration_ms, vectors_per_second);
-}
-
-template <typename T>
-void run_test_round_worker(const shared_ptr<spdlog::logger> &log, const size_t repetitions, const Worker &worker,
-                    const vector_t& query, const size_t expected_best_idx, const float expected_best_score) {
-
-    const DotProductVisitor<T> visitor {};
-    auto duration_ms = static_cast<size_t>(0);
-
-    for (size_t repetition = 0; repetition < repetitions; ++repetition) {
-        auto start_time = std::chrono::_V2::system_clock::now();
-        auto results = worker.create_result_buffer();
-
-        // Keep track of the total sum for validation.
-        auto best_match = 0.0f;
-        auto best_match_idx = static_cast<size_t>(0);
-
-        worker.accept(visitor, query, results);
-
-        auto end_time = std::chrono::_V2::system_clock::now();
-        auto local_duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-        duration_ms += local_duration_ms;
-
-        // TODO: This should eventually be part of the worker, otherwise we're going through the lists twice.
-        for (auto chunk_result : results) {
-            for (size_t vector_idx = 0; vector_idx < NUM_VECTORS; ++vector_idx) {
-                const auto score = chunk_result.second->scores[vector_idx];
-                if (score > best_match) {
-                    best_match = score;
-                    best_match_idx = vector_idx;
-                }
-            }
-        }
-
-        auto local_vectors_per_second = static_cast<float>(NUM_VECTORS) * MS_TO_S / static_cast<float>(local_duration_ms);
-        log->debug("- Round {}/{} matched {} at {} (expected {} at {}); took {} ms ({} vectors/s)",
-                   repetition + 1, repetitions,
-                   best_match, best_match_idx,
-                   expected_best_score, expected_best_idx,
-                   local_duration_ms, local_vectors_per_second);
-    }
-
-    const size_t num_vectors = repetitions * NUM_VECTORS;
-    auto vectors_per_second = static_cast<float>(num_vectors) * MS_TO_S / static_cast<float>(duration_ms);
-    log->info("- Processed {} vectors in {} ms ({} vectors/s)",
-              num_vectors, duration_ms, vectors_per_second);
 }
 
 void what(const shared_ptr<spdlog::logger> &log) {
