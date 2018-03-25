34fe777dbb2f3329f22e294ff55278f9025d6f71
Markus Mayer <widemeadows@gmail.com>
2018-03-25 14:42:50 +0200

    Add specialized result casting

--- src/libfirestorm/include/firestorm/engine/mapreduce/any_result_t.h
+++ src/libfirestorm/include/firestorm/engine/mapreduce/any_result_t.h
@@ -6,8 +6,19 @@
 #define PROJECT_ANY_RESULT_T_H
 
 #include <any>
+#include <typeindex>
 
 namespace firestorm {
+
+    template<typename T>
+    class has_result_t {
+    public:
+        virtual ~has_result_t() noexcept = default;
+
+        virtual const T& get() const noexcept = 0;
+
+        inline std::type_index value_type() const { return typeid(T); };
+    };
 
     class any_result_t {
     public:
@@ -15,19 +26,24 @@ namespace firestorm {
 
         virtual std::any as_any() const noexcept = 0;
 
-        template<class T>
+        template<typename T>
         inline T any_cast() const {
-            // TODO: Can we specialize this?
+            const auto ptr = dynamic_cast<const has_result_t<T>*>(this);
+            if (ptr != nullptr) {
+                return ptr->get();
+            }
+
             return std::any_cast<T>(as_any());
         }
     };
 
-    template<class T>
-    class typed_result_t : public virtual any_result_t {
+    template<typename T>
+    class typed_result_t : public virtual any_result_t,
+                           public virtual has_result_t<T> {
     public:
         ~typed_result_t() noexcept override = default;
 
-        virtual const T& get() const noexcept = 0;
+        const T& get() const noexcept override = 0;
 
         inline std::any as_any() const noexcept final { return get(); }
     };
